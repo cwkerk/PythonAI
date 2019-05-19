2c6bd3bda3a2491aace4b6b5635bf340aa8271a4
Chin Wee Kerk <kerkchinwee@hotmail.co.uk>
2019-05-19 17:22:09 +0800

    Sanitizing Sarsa.py and combine SemiGradientSarsa.py into it

--- Sarsa.py
+++ Sarsa.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plot
-from numpy import argmax, array, ndarray, where, zeros
+from numpy import argmax, array, dot, ndarray, where, zeros
 from numpy.random import choice, random
 
 from EpsilonGreedy import epsilon_greedy
@@ -16,7 +16,7 @@ def _take_action(π):
 
 def sarsa(**kwargs):
 
-    """Basic SARSA algorithm without any optimization """
+    """Basic SARSA algorithm without any optimization"""
 
     S = kwargs.pop("states")
     if not isinstance(S, ndarray) or not isinstance(S.size, int):
@@ -26,6 +26,7 @@ def sarsa(**kwargs):
     T = kwargs.pop("terminate_state")
     if T not in S:
         raise Exception("terminal state is not one of the state")
+    t, = where(S == T)
 
     A = kwargs.pop("actions")
     if not isinstance(A, ndarray) or not isinstance(A.size, int):
@@ -61,9 +62,9 @@ def sarsa(**kwargs):
         raise Exception("epsilon must be float typed")
 
     try:
-        g = kwargs.pop("gamma")
+        γ = kwargs.pop("gamma")
     except:
-        g = 0.8
+        γ = 0.8
     if not isinstance(e, float):
         raise Exception("gamma must be integer typed")
 
@@ -75,7 +76,6 @@ def sarsa(**kwargs):
         raise Exception("episode size must be int typed")
 
     Q = zeros([NS, NA])
-    t, = where(S == T)
 
     td_errors = []
 
@@ -89,7 +89,7 @@ def sarsa(**kwargs):
             s_prime = argmax(P[s, a])
             π_prime = epsilon_greedy(actions=A, action_values=Q, state_index=int(s_prime))
             a_prime = _take_action(π_prime)
-            td_error = R[s, a, s_prime] + g * Q[s_prime, a_prime] - Q[s, a]
+            td_error = R[s, a, s_prime] + γ * Q[s_prime, a_prime] - Q[s, a]
             # Q(s, a) ← Q(s, a) + α[􏰄R + γQ(s′, a′) − Q(s, a)􏰅]
             Q[s, a] = Q[s, a] + a * td_error
             s = s_prime
@@ -101,7 +101,114 @@ def sarsa(**kwargs):
     return Q
 
 
+def semi_gradient_sarsa(**kwargs):
+
+    """Basic SARSA algorithm with semi gradient optimization using linear method"""
+
+    S = kwargs.pop("states")
+    if not isinstance(S, ndarray) or not isinstance(S.size, int):
+        raise Exception("states are needed to be one dimensional numpy.ndarray object")
+    NS = S.size
+
+    T = kwargs.pop("terminate_state")
+    if T not in S:
+        raise Exception("terminal state is not one of the state")
+    t, = where(S == T)
+
+    A = kwargs.pop("actions")
+    if not isinstance(A, ndarray) or not isinstance(A.size, int):
+        raise Exception("actions are needed to be one dimensional numpy.ndarray object")
+    NA = A.size
+
+    NW = kwargs.pop("weight_size")
+    if not isinstance(NW, int):
+        raise Exception("weight size should be int typed")
+    W = random(NW)
+
+    try:
+        P = kwargs.pop("transitions")
+    except:
+        P = random([NS, NA, NS])
+    if not isinstance(P, ndarray) or P.shape != tuple([NS, NA, NS]):
+        raise Exception("transitions are needed to be numpy.ndarray object with shape of [NS, NA, NS]")
+
+    try:
+        R = kwargs.pop("rewards")
+    except:
+        R = random([NS, NA, NS])
+    if not isinstance(R, ndarray) or R.shape != tuple([NS, NA, NS]):
+        raise Exception("rewards are needed to be numpy.ndarray object with shape of [NS, NA, NS]")
+
+    try:
+        a = kwargs.pop("alpha")
+    except:
+        a = 0.8
+    if not isinstance(a, float):
+        raise Exception("alpha must be float typed")
+
+    try:
+        e = kwargs.pop("epsilon")
+    except:
+        e = 0.1
+    if not isinstance(e, float):
+        raise Exception("epsilon must be float typed")
+
+    try:
+        γ = kwargs.pop("gamma")
+    except:
+        γ = 0.8
+    if not isinstance(e, float):
+        raise Exception("gamma must be integer typed")
+
+    try:
+        episode_size = kwargs.pop("episode_size")
+    except:
+        episode_size = 100
+    if not isinstance(episode_size, int):
+        raise Exception("episode size must be int typed")
+
+    Q = zeros([NS, NA, NW])
+
+    td_errors = []
+
+    for episode in range(episode_size):
+
+        s = choice(range(NS))
+        π = epsilon_greedy(actions=A, action_values=dot(Q, W), state_index=int(s))
+        a = _take_action(π)
+        q = dot(W, Q[s, a])
+
+        while s != t[0]:
+            s_prime = argmax(P[s, a])
+            r_prime = R[s, a, s_prime]
+            q_gradient = sum(Q[s, a])
+
+            if s_prime == t[0]:
+                W = W + a * (r_prime - q) * q_gradient
+                break
+            else:
+                π_prime = epsilon_greedy(actions=A, action_values=dot(Q, W), state_index=int(s_prime))
+                a_prime = _take_action(π_prime)
+                q_prime = dot(W, Q[s_prime, a_prime])
+                td_error = r_prime + γ * q_prime - q
+                # W ← W + α[􏰄R + γQ(s′, a′) − Q(s, a)􏰅] * ∇Q(s, a)
+                W = W + a * td_error * q_gradient
+                s = s_prime
+                a = a_prime
+                q = q_prime
+
+        try:
+            td_errors.append(td_error)
+        except:
+            print("No TD error is found as the first step of this episode is terminal state.")
+    plot.plot(td_errors)
+    plot.show()
+
+    return Q
+
+
 if __name__ == "__main__":
     S = array(["s1", "s2", "s3", "s4", "s5"])
     A = array(["a1", "a2", "a3", "a4"])
     sarsa(states=S, actions=A, terminate_state="s5")
+    semi_gradient_sarsa(states=S, actions=A, terminate_state="s5", weight_size=3)
