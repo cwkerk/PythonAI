2e16a3e3180f008b647fba417a78048704925578
Kerk Chin Wee <kerkchinwee@hotmail.co.uk>
2019-07-27 17:05:55 +0800

    clean up MultiLayerPerceptron.py

--- MultiLayerPerceptron.py
+++ MultiLayerPerceptron.py
@@ -2,16 +2,16 @@ from matplotlib import pyplot
 from numpy import array, dot, exp, outer, random, zeros
 
 
-def _cost_derivative(output, expected):
-    return output - expected
+def _cost_derivative(output, target):
+    return output - target
 
 
 def sigmoid(z):
     return 1 / (1 + exp(-z))
 
 
-def sigmoid_derivative(z):
-    return sigmoid(z) * (1 - sigmoid(z))
+def sigmoid_derivative(a):
+    return a * (1 - a)
 
 
 class Network:
@@ -26,12 +26,10 @@ class Network:
         self.weights = [random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
     # both expected_outputs is data list of size same as output layer
-    def _back_propagation(self, expected_activation, feed_forward_result):
+    def _back_propagation(self, targets, outputs):
         nabla_biases = [zeros(bias.shape) for bias in self.biases]
         nabla_weights = [zeros(weight.shape) for weight in self.weights]
-        activations = feed_forward_result["activations"]
-        outputs = feed_forward_result["outputs"]
-        delta = _cost_derivative(activations[-1], expected_activation) * sigmoid_derivative(outputs[-1])
+        delta = _cost_derivative(outputs[-1], targets) * sigmoid_derivative(outputs[-1])
         # cost to correct output bias,
         # ∂(error)/∂(output_bias) = ∂(error)/∂(output)
         nabla_biases[-1] = delta
@@ -39,7 +37,7 @@ class Network:
         # ∂(error)/∆(output_weight)
         #   = ∂(error)/∂(output) * ∂(output)/∂(weight)
         #   = outer product of ∂(error)/∂(output) & ∂(output)/∂(weight)
-        nabla_weights[-1] = outer(delta, activations[-2])
+        nabla_weights[-1] = outer(delta, outputs[-2])
         # From second last layer to first layer
         for i in range(2, len(self.sizes)):
             # compute delta,
@@ -49,11 +47,11 @@ class Network:
             #   = delta * weight * sigmoid_derivative(current_output)
             delta = dot(delta, self.weights[-i + 1]) * sigmoid_derivative(outputs[-i])
             nabla_biases[-i] = delta
-            nabla_weights[-i] = outer(delta, activations[-i - 1])
+            nabla_weights[-i] = outer(delta, outputs[-i - 1])
         return nabla_biases, nabla_weights
 
-    def _stochastic_gradient_descent(self, expected_activations, feed_forward_result, learning_rate):
-        nabla_biases, nabla_weights = self._back_propagation(expected_activations, feed_forward_result)
+    def _stochastic_gradient_descent(self, targets, outputs, learning_rate):
+        nabla_biases, nabla_weights = self._back_propagation(targets, outputs)
         self.biases = [b - learning_rate * nb for b, nb in zip(self.biases, nabla_biases)]
         self.weights = [w - learning_rate * nw for w, nw in zip(self.weights, nabla_weights)]
 
@@ -67,24 +65,20 @@ class Network:
             outputs.append(z)
             a = sigmoid(z)
             activations.append(a)
-        return {"activations": activations, "outputs": outputs}
+        return activations
 
-    def train_with_stochastic_gradient_descent(self, expected_output, training_input, learning_rate=0.85):
+    def train_with_stochastic_gradient_descent(self, training_labels, training_inputs, learning_rate=0.85):
         epoch = 0
         errors = []
-        expected_activation = sigmoid(expected_output)
-        sigmoidized_input = sigmoid(training_input)
-        feed_forward_result = self.feed_forward(sigmoidized_input)
-        final_activation = feed_forward_result["activations"][-1]
-        training_error = 0.5 * sum([abs(e - o) ** 2 for e, o in zip(expected_activation, final_activation)])
-        errors.append(training_error)
+        targets = sigmoid(training_labels)
+        inputs = sigmoid(training_inputs)
+        training_error = self.threshold
         while epoch <= self.max_epoch and training_error >= self.threshold:
             epoch += 1
-            self._stochastic_gradient_descent(expected_activation, feed_forward_result, learning_rate)
-            feed_forward_result = self.feed_forward(sigmoidized_input)
-            final_activation = feed_forward_result["activations"][-1]
-            training_error = 0.5 * sum([abs(e - o) ** 2 for e, o in zip(expected_activation, final_activation)])
+            outputs = self.feed_forward(inputs)
+            training_error = 0.5 * sum([abs(e - o) ** 2 for e, o in zip(targets, outputs[-1])])
             errors.append(training_error)
+            self._stochastic_gradient_descent(targets, outputs, learning_rate)
         return errors
 
 
@@ -102,8 +96,3 @@ if __name__ == "__main__":
     errors_3 = net.train_with_stochastic_gradient_descent(output, input, learning_rate=1.15)
     print("Final Error: {}".format(errors[-1]))
     pyplot.show()
-    # test
-    input_2 = array([0.7, 0.2])
-    result = net.feed_forward(input_2)
-    output_2 = result["outputs"][-1]
-    print(output_2)
